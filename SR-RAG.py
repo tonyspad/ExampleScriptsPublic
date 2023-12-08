99c43458dacbe1392ec127a617f572c6c3bf1ace
tonyspad <96081282+tonyspad@users.noreply.github.com>
2023-12-08 12:11:43 -0500

    Update SR-RAG.py
    RAG example for candidate Ex

--- SR-RAG.py
+++ SR-RAG.py
@@ -1,42 +1,3 @@
-Last login: Thu Dec  7 22:17:08 on ttys002
-anthonyspadafino@anthonys-mbp-2 ~ % mkdir Python1
-mkdir: Python1: File exists
-anthonyspadafino@anthonys-mbp-2 ~ % mkdir Python2
-anthonyspadafino@anthonys-mbp-2 ~ % cd Python2
-anthonyspadafino@anthonys-mbp-2 Python2 % vim SR-RAG.py
-anthonyspadafino@anthonys-mbp-2 Python2 % vim SR-RAG.py
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import os
 import pinecone
 from langchain.llms import OpenAI
@@ -50,24 +11,46 @@ from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import PyMuPDFLoader
 
-os.environ["OPENAI_API_KEY"] = ("open-ai-api-key-here")
-os.environ["PC_API_KEY"] = ("pc-api-key-here")
-embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["PC_API_KEY"] = "286f7f16-6ea7-4cd9-9e22-65419c03aca3"
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY']) 
 pinecone.init(
-            api_key= os.environ['PC_API_KEY'],
-            environment= ("pc-env-here")
+            api_key= os.environ['PC_API_KEY'], 
+            environment= 'gcp-starter'
 )
 
 #loader = PyMuPDFLoader("./docs/example.pdf")
 #documents = loader.load()
 
 vectorstore = FAISS.from_texts(
-    ["Our Values: To achieve our mission of connecting people to jobs they love, we hire people who believe in and embody our values. We win and lose as a team, act as the CEO of our jobs and strive for continuous improvement with an emphasis on making an impact. As a new team member, these four areas will be critical to your success at SmartRecruiters.",
+    ["Our Values: To achieve our mission of connecting people to jobs they love, we hire people who believe in and embody our values. We win and lose as a team, act as the CEO of our jobs and strive for continuous improvement with an emphasis on making an impact. As a new team member, these four areas will be critical to your success at SmartRecruiters.", 
      "Our benefits: We’ve got you covered with a competitive benefits package. Take time when you need it with unlimited PTO. Recharge during our twice annual Company Shutdown. Grow with us and enjoy our generous equity program. Invest in your wellness and receive a wellness stipend. Prepare for the future with robust retirement options by region. Give back to your community using paid time off to volunteer. Grow your career through our leadership development programs"
-    ],
+    ], 
     embedding=OpenAIEmbeddings()
 )
 retriever = vectorstore.as_retriever()
 
 model = ChatOpenAI()
-"SR-RAG.py" 56L, 2511B
+context = "You are a recruiter at Recruiting & HR SaaS platform SmartRecruiters.com. You help candidates get the information they need about open roles."
+
+template = """Answer the question using following context:
+{context}
+
+Question: {question}
+
+Answer in the following language: {language}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {
+        "context": itemgetter("question") | retriever,
+        "question": itemgetter("question"),
+        "language": itemgetter("language"),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+chain.invoke({"question": "Summarize the benefits at SmartRecruiters", "language": "german"})
