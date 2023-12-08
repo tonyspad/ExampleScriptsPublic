cb170118360b7a0ef4a7bf9a15350c91487ae3ae
tonyspad <96081282+tonyspad@users.noreply.github.com>
2023-12-08 12:11:02 -0500

    Update SR-JD-bot.py
    Job description creator bot

--- SR-JD-bot.py
+++ SR-JD-bot.py
@@ -1,51 +1,38 @@
-Last login: Thu Dec  7 20:57:36 on ttys000
-anthonyspadafino@anthonys-mbp-2 ~ % mkdir Python1
-anthonyspadafino@anthonys-mbp-2 ~ % cd Python1
-anthonyspadafino@anthonys-mbp-2 Python1 % vim SR-JD-bot.py
 
+import os
+import openai
+from openai import OpenAI
 
+client = openai.OpenAI(
+    api_key= ('OPEN-AI-KEY-HERE'),
+    organization= ('org-KEY-HERE')
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def MLE():
+    header = "Staff Machine Learning Engineer"
+    subheader = "New York, New York | San Francisco, CA"
+    about = (
+        "About the Role\n"
+        "A marketplace can grow if there is a balance between supply and demand. Selection is the key differentiating factor which will bring more eaters to the platform and create a flywheel effect of generating order volume which will add value to merchants to stay and grow with Uber. The team’s mission is to improve selection from both eater and merchant lens by executing on strategic growth and profitability levers which will keep the flywheel moving and create the network effect.\n"
+        
+    )
+    comp = (
+        "Compensation\n"
+        "FOr San Francisco, CA-based roles: The base salary range for this role is $174,000 per year - $193,500 per year./n" 
+        "For New York, NY-based roles: The base salary range for this role is USD$207,000 per year - USD$230,000 per year./n"
+        "You will be eligible to participate in Uber's bonus program, and may be offered an equity award & other types of comp. You will also be eligible for various benefits. More details can be found at the following link https://www.uber.com/careers/benefits.\n"
+        
+    )
+    qualifications = (
+        "* Bachelor in Computer Science, Engineering, Mathematics or related field and 8-years full-time as a Machine Learning Engineer or Software Engineer, working on building data pipelines, training and deploying machine learning models in production on a daily basis\n"
         "* Experience with writing, releasing, and working in large distributed systems, deep learning or/and natural language processing is a big plus.\n"
         "* Experience analyzing a feature in production using analytics and experiments.\n"
         "* Experience in using Python (pandas, scipy, or numpy, etc) for scientific computations and object-oriented programming.\n"
         "* Experience in using SQL (Presto, Hive, or Spark, etc) for data querying and ETL.\n"
         "* Experience in applying machine learning models to solve real-world problems.\n"
-
+        
     )
-
+    
 
 
     return f"{header}\n\n{subheader}\n\n{about}\n\n{comp}\n\n{qualifications}"
@@ -68,10 +55,4 @@ def job_desc(prompt):
     reply_content = completion.choices[0].message.content
     print(reply_content)
 
-job_desc("Sr. Product Manager"")
-Entering Ex mode.  Type "visual" to go to Normal mode.
-:
-E501: At end-of-file
-:.wq
-E140: Use ! to write partial buffer
-:
+job_desc("Operations Manager")
